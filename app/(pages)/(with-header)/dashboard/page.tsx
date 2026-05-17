46e60019caeb0200a80956015646ccca5584057c
fhel alexandre <fhelbete007@gmail.com>
2026-05-18 01:46:51 +0800

    Edit: Site icon and added metadata to pages

--- app/(pages)/(with-header)/dashboard/page.tsx
+++ app/(pages)/(with-header)/dashboard/page.tsx
@@ -2,9 +2,15 @@
 
 import { Box, Typography } from "@mui/material";
 import { useSession } from "next-auth/react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard - Inbox Cleanse",
+  description:
+    "Take control of your inbox. Unsubscribe from unwanted emails with ease and keep your inbox clean and organized.",
+};
+
 export default function DashboardPage() {
-  const { data: session } = useSession();
-  console.log("Session data on Dashboard:", session);
   return (
     <Box>
       <Typography children="Dashboard" variant="h4" gutterBottom />
